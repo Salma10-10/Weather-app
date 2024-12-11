e64fef5b41555d9285a116d7b86574a475eb22fc
Salma <elfalahsalma3@gmail.com>
2024-12-11 13:35:26 -0500

    added api key

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@
 import Navbar from "@/app/components/Navbar";
 import Image from "next/image";
 
+//https://api.openweathermap.org/data/2.5/forecast?q=montreal&appid=f8308b9212f84de2c4c2f1b35117be2b&cnt=1
+
+
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen ">
